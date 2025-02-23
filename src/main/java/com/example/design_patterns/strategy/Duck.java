f94a8cdb72ca8e835639fadd686c55926f33b700
flyin <flying.falcon@outlook.com>
2025-02-23 15:43:42 +0100

    [1] create abstract class Duck having abstract methods quack(), display() and fly()

---
+++ src/main/java/com/example/design_patterns/strategy/Duck.java
@@ -0,0 +1,9 @@
+package com.example.design_patterns.strategy;
+
+public abstract class Duck {
+
+    public abstract void quack();
+    public abstract void display();
+    public abstract void fly();
+
+}
