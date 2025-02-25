2d5a5562d4dc930e9591e8975e906f9176735bbd
flyin <flying.falcon@outlook.com>
2025-02-25 22:11:44 +0100

    [8] remove one of the observers, the forecast display observer, after dataset updates
    The forecast display observer is now removed before simulating the new weather measurements.

--- src/main/java/com/example/design_patterns/DesignPatternsApplication.java
+++ src/main/java/com/example/design_patterns/DesignPatternsApplication.java
@@ -26,6 +26,8 @@ public class DesignPatternsApplication {
 		weatherData.setMeasurements(82,70,29.2f);
 		weatherData.setMeasurements(78,90,29.2f);
 
+		weatherData.removeObserver(forecastDisplay);
+		weatherData.setMeasurements(62,90,28.1f);
 	}
 
 }
