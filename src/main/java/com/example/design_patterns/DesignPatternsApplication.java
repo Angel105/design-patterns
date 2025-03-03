da00151a8deab41ced7c9725a27c5cf05058690e
flyin <flying.falcon@outlook.com>
2025-03-03 14:16:23 +0100

    [19] add a classic Singleton pattern implementation and example usage
    Introduce a non-thread-safe Singleton class with a description method. Refactor main application to demonstrate Singleton usage, replacing previous PizzaStore order examples. This provides a clean implementation of the classic Singleton design pattern.

--- src/main/java/com/example/design_patterns/DesignPatternsApplication.java
+++ src/main/java/com/example/design_patterns/DesignPatternsApplication.java
@@ -4,6 +4,7 @@ import com.example.design_patterns.factory.Pizza;
 import com.example.design_patterns.factory.PizzaStore;
 import com.example.design_patterns.factory.store.ChicagoPizzaStore;
 import com.example.design_patterns.factory.store.NYPizzaStore;
+import com.example.design_patterns.singleton.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -17,35 +18,8 @@ public class DesignPatternsApplication {
     public static void main(String[] args) {
         SpringApplication.run(DesignPatternsApplication.class, args);
 
-        PizzaStore nyStore  = new NYPizzaStore();
-        PizzaStore chicagoStore = new ChicagoPizzaStore();
-
-        Pizza pizza;
-
-        pizza = nyStore.orderPizza("cheese");
-        logger.info("Ethan ordered a {}", pizza);
-
-        pizza = chicagoStore.orderPizza("cheese");
-        logger.info("Joel ordered a {}", pizza);
-
-        pizza = nyStore.orderPizza("clam");
-        logger.info("Ethan ordered a {}", pizza);
-
-        pizza = chicagoStore.orderPizza("clam");
-        logger.info("Joel ordered a {}", pizza);
-
-        pizza = nyStore.orderPizza("pepperoni");
-        logger.info("Ethan ordered a {}", pizza);
-
-        pizza = chicagoStore.orderPizza("pepperoni");
-        logger.info("Joel ordered a {}", pizza);
-
-        pizza = nyStore.orderPizza("veggie");
-        logger.info("Ethan ordered a {}", pizza);
-
-        pizza = chicagoStore.orderPizza("veggie");
-        logger.info("Joel ordered a {}", pizza);
+        Singleton singleton = Singleton.getInstance();
+        logger.info(singleton.getDescription());
     }
-
 
 }
