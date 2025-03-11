33f810ed2b295f29ef0f46329a61253f67f6e0c5
flyin <flying.falcon@outlook.com>
2025-03-11 17:19:56 +0100

    [40] use abstract type references.
    Updated object declarations to use the abstract type `CaffeineBeverageWithHook` to improve flexibility and adherence to polymorphism principles.

--- src/main/java/com/example/design_patterns/DesignPatternsApplication.java
+++ src/main/java/com/example/design_patterns/DesignPatternsApplication.java
@@ -1,9 +1,6 @@
 package com.example.design_patterns;
 
-import com.example.design_patterns.templatemethod.CoffeeWithHook;
-import com.example.design_patterns.templatemethod.ConsoleInputHandler;
-import com.example.design_patterns.templatemethod.InputHandler;
-import com.example.design_patterns.templatemethod.TeaWithHook;
+import com.example.design_patterns.templatemethod.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -17,8 +14,8 @@ public class DesignPatternsApplication {
     public static void main(String[] args) {
         SpringApplication.run(DesignPatternsApplication.class, args);
         InputHandler inputHandler = new ConsoleInputHandler();
-        TeaWithHook tea = new TeaWithHook(inputHandler);
-        CoffeeWithHook coffee = new CoffeeWithHook(inputHandler);
+        CaffeineBeverageWithHook tea = new TeaWithHook(inputHandler);
+        CaffeineBeverageWithHook coffee = new CoffeeWithHook(inputHandler);
 
         logger.info("\nMaking tea...");
         tea.prepareRecipe();
