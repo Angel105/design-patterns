c5859ea9563ccfa93e12f6a50a73d8544e9cfdda
flyin <flying.falcon@outlook.com>
2025-03-19 17:19:02 +0100

    [52] add GumballMachine implementation with basic State behavior
    Introduced a new `GumballMachine` class implementing basic state management and transitions for a gumball vending machine. Updated `DesignPatternsApplication` to demonstrate the functionality through various machine operations and state changes.

--- src/main/java/com/example/design_patterns/DesignPatternsApplication.java
+++ src/main/java/com/example/design_patterns/DesignPatternsApplication.java
@@ -1,9 +1,6 @@
 package com.example.design_patterns;
 
-import com.example.design_patterns.collections.composite.menuiterator.Menu;
-import com.example.design_patterns.collections.composite.menuiterator.MenuComponent;
-import com.example.design_patterns.collections.composite.menuiterator.MenuItem;
-import com.example.design_patterns.collections.composite.menuiterator.Waitress;
+import com.example.design_patterns.state.GumballMachine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -18,127 +15,36 @@ public class DesignPatternsApplication {
     public static void main(String[] args) {
         SpringApplication.run(DesignPatternsApplication.class, args);
 
+        GumballMachine gumballMachine = new GumballMachine(5);
+        logger.info("{}", gumballMachine);
 
-        MenuComponent pancakeHouseMenu = new Menu("PANCAKE HOUSE MENU", "Breakfast");
-        MenuComponent dinerMenu = new Menu("DINNER MENU", "Lunch");
-        MenuComponent cafeMenu = new Menu("CAFE MENU", "Dinner");
-        MenuComponent dessertMenu = new Menu("DESSERT MENU", "Dessert of course!");
-        MenuComponent coffeeMenu = new Menu("COFFEE MENU", "Stuff to go with your afternoon coffee");
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
 
-        MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
+        logger.info("{}", gumballMachine);
 
-        allMenus.add(pancakeHouseMenu);
-        allMenus.add(dinerMenu);
-        allMenus.add(cafeMenu);
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
 
-        pancakeHouseMenu.add(new MenuItem(
-                "K&B's Pancake Breakfast",
-                "Pancakes with scrambled eggs and toast",
-                true,
-                2.99));
-        pancakeHouseMenu.add(new MenuItem(
-                "Regular Pancake Breakfast",
-                "Pancakes with fried eggs, sausage",
-                false,
-                2.99));
-        pancakeHouseMenu.add(new MenuItem(
-                "Blueberry Pancakes",
-                "Pancakes made with fresh blueberries, and blueberry syrup",
-                true,
-                3.49));
-        pancakeHouseMenu.add(new MenuItem(
-                "Waffles",
-                "Waffles with your choice of blueberries or strawberries",
-                true,
-                3.59));
+        logger.info("{}", gumballMachine);
 
-        dinerMenu.add(new MenuItem(
-                "Vegetarian BLT",
-                "(Fakin') Bacon with lettuce & tomato on whole wheat",
-                true,
-                2.99));
-        dinerMenu.add(new MenuItem(
-                "BLT",
-                "Bacon with lettuce & tomato on whole wheat",
-                false,
-                2.99));
-        dinerMenu.add(new MenuItem(
-                "Soup of the day",
-                "A bowl of the soup of the day, with a side of potato salad",
-                false,
-                3.29));
-        dinerMenu.add(new MenuItem(
-                "Hot Dog",
-                "A hot dog, with saurkraut, relish, onions, topped with cheese",
-                false,
-                3.05));
-        dinerMenu.add(new MenuItem(
-                "Steamed Veggies and Brown Rice",
-                "Steamed vegetables over brown rice",
-                true,
-                3.99));
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
 
-        dinerMenu.add(new MenuItem(
-                "Pasta",
-                "Spaghetti with marinara sauce, and a slice of sourdough bread",
-                true,
-                3.89));
+        logger.info("{}", gumballMachine);
 
-        dinerMenu.add(dessertMenu);
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
 
-        dessertMenu.add(new MenuItem(
-                "Apple Pie",
-                "Apple pie with a flakey crust, topped with vanilla icecream",
-                true,
-                1.59));
-
-        dessertMenu.add(new MenuItem(
-                "Cheesecake",
-                "Creamy New York cheesecake, with a chocolate graham crust",
-                true,
-                1.99));
-        dessertMenu.add(new MenuItem(
-                "Sorbet",
-                "A scoop of raspberry and a scoop of lime",
-                true,
-                1.89));
-
-        cafeMenu.add(new MenuItem(
-                "Veggie Burger and Air Fries",
-                "Veggie burger on a whole wheat bun, lettuce, tomato, and fries",
-                true,
-                3.99));
-        cafeMenu.add(new MenuItem(
-                "Soup of the day",
-                "A cup of the soup of the day, with a side salad",
-                false,
-                3.69));
-        cafeMenu.add(new MenuItem(
-                "Burrito",
-                "A large burrito, with whole pinto beans, salsa, guacamole",
-                true,
-                4.29));
-
-        cafeMenu.add(coffeeMenu);
-
-        coffeeMenu.add(new MenuItem(
-                "Coffee Cake",
-                "Crumbly cake topped with cinnamon and walnuts",
-                true,
-                1.59));
-        coffeeMenu.add(new MenuItem(
-                "Bagel",
-                "Flavors include sesame, poppyseed, cinnamon raisin, pumpkin",
-                false,
-                0.69));
-        coffeeMenu.add(new MenuItem(
-                "Biscotti",
-                "Three almond or hazelnut biscotti cookies",
-                true,
-                0.89));
-
-        Waitress waitress = new Waitress(allMenus);
-
-        waitress.printVeganMenu();
+        logger.info("{}", gumballMachine);
     }
 }
